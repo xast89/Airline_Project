3f3678a22a29e35244eb42fff8a3ce91607b8b3d
Cyprian Foremny <cforemny@gmail.com>
2017-01-25 14:45:36 +0100

    Finding activeSegment  move from AgeAtSegment  to new class ActiveSegment

--- src/main/java/com/learning/analyzer/ageAnalizer/ActiveSegment.java
+++ src/main/java/com/learning/analyzer/ageAnalizer/ActiveSegment.java
@@ -3,21 +3,20 @@ package com.learning.analyzer.ageAnalizer;
 import com.learning.structure.booking.Passenger;
 import com.learning.structure.booking.Segment;
 
-import java.util.List;
+import static com.learning.structure.util.FlightStatus.ACTIVE;
 
 /**
  * Created by Cyprian on 2017-01-25.
  */
 public class ActiveSegment {
 
-    public Segment  findActiveSegment(List<Segment> segmentList, Passenger passenger){
-        for (Segment segment : passenger.getSegmentList() ) {
-            if(segment.isActive()){
+    public Segment findActiveSegment(Passenger passenger) {
+        for (Segment segment : passenger.getSegmentList()) {
+            if (segment.isActive()) {
                 return segment;
             }
-
         }
-
         return null;
+
     }
 }
--- src/main/java/com/learning/analyzer/ageAnalizer/AgeAtSegment.java
+++ src/main/java/com/learning/analyzer/ageAnalizer/AgeAtSegment.java
@@ -13,28 +13,25 @@ import static org.joda.time.Days.daysBetween;
  */
 public class AgeAtSegment {
 
+    private ActiveSegment activeSegment;
+
+    public AgeAtSegment() {
+        this.activeSegment = new ActiveSegment();
+    }
 
     public int countDays(Passenger passenger, Calendar birthDayFromPassport) {
-        int days;
         //TODO: a moze wartosc sie zastanowić, by ta metodka dostawala pierwszy aktywny segment Pasazera i wyliczala
         //TODO: tylko jego wiek ? W sensie reszte logiki wyrzucic poza ta matode (moze zrobic osobna?)
 
-        for (Segment segment : passenger.getSegmentList()) {
-            if (segment.isActive()) {
-
-                DateTime passengerBirthday = new DateTime(birthDayFromPassport.get(Calendar.YEAR), birthDayFromPassport.get(Calendar.MONTH), birthDayFromPassport.get(Calendar.DAY_OF_MONTH), 0, 0, 0, 0);
-                DateTime actualSegmentDate = new DateTime(segment.getDepartureDate().get(Calendar.YEAR), segment.getDepartureDate().get(Calendar.MONTH), segment.getDepartureDate().get(Calendar.DAY_OF_MONTH), 0, 0, 0, 0);
-                days = daysBetween(passengerBirthday, actualSegmentDate).getDays();
-                //TODO: zamienic na logowanie https://www.tutorialspoint.com/log4j/log4j_sample_program.htm
-                System.out.println("W dni wylotu w segmencie pasazer ma lat: " + days / 365);
-                return days;
-            }
-        }
-
-        return 0;
-
+        int days;
+        Segment segment = activeSegment.findActiveSegment(passenger);
+        DateTime passengerBirthday = new DateTime(birthDayFromPassport.get(Calendar.YEAR), birthDayFromPassport.get(Calendar.MONTH), birthDayFromPassport.get(Calendar.DAY_OF_MONTH), 0, 0, 0, 0);
+        DateTime actualSegmentDate = new DateTime(segment.getDepartureDate().get(Calendar.YEAR), segment.getDepartureDate().get(Calendar.MONTH), segment.getDepartureDate().get(Calendar.DAY_OF_MONTH), 0, 0, 0, 0);
+        days = daysBetween(passengerBirthday, actualSegmentDate).getDays();
+        //TODO: zamienic na logowanie https://www.tutorialspoint.com/log4j/log4j_sample_program.htm
+        System.out.println("W dni wylotu w segmencie pasazer ma lat: " + days / 365);
+        return days;
     }
-
 }
 
 
